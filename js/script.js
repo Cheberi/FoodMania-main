e246c55d77609c2c7aa4957eec89dfc40a3cf961
Cheberi <meshackkemboi011@gmail.com>
2024-03-28 12:12:05 +0300

    updated script.js

--- js/script.js
+++ js/script.js
@@ -5,20 +5,45 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
 
-///////////////////////////////////////////////////////////
-// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-///////////////////////////////////////////////////////////
-// Make mobile navigation work
+
+const togglebtns = document.querySelectorAll('.nav-toggle');
+const sideBar = document.querySelector('div.side-navbar');
+const sidebarLinks = document.querySelectorAll('.side-navbar li');
+const threeDotMenu = document.querySelector('div.nav-menu-icon');
+const navMenu = document.querySelector('div.nav-menu');
+const backgroundFader = document.getElementsByClassName('background-fader')[0];
+
+
+function toggleSidebar() {
+  sideBar.classList.toggle('show-side-navbar');
+  backgroundFader.classList.toggle('fade-background');
+}
+
+togglebtns.forEach(togglebtn => {
+  togglebtn.onclick = function() {
+    toggleSidebar();
+  };
+});
+
+backgroundFader.onclick = () => {
+  toggleSidebar(); 
+};
+
+sidebarLinks.forEach(link => {
+  link.onclick = e => {
+    e.preventDefault();
+    toggleSidebar();
+  };
+});
+
+threeDotMenu.onclick = function() {
+  navMenu.classList.toggle('show-nav-menu');
+};
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
@@ -27,8 +52,7 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
+
 
 const allLinks = document.querySelectorAll("a:link");
 
@@ -56,8 +80,7 @@ allLinks.forEach(function (link) {
   });
 });
 
-///////////////////////////////////////////////////////////
-// Sticky navigation
+
 
 const sectionHeroEl = document.querySelector(".section-hero");
 
@@ -83,8 +106,7 @@ const obs = new IntersectionObserver(
 );
 obs.observe(sectionHeroEl);
 
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
+
 function checkFlexGap() {
   var flex = document.createElement("div");
   flex.style.display = "flex";
@@ -103,55 +125,3 @@ function checkFlexGap() {
 }
 checkFlexGap();
 
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
